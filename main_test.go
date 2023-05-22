4779d0e248acc39dcf6ccd55722376614187242b
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-22 17:59:54 +0100

    finished unit test for switch statement
    unit test covering all bases and methods for the switch statement

--- main_test.go
+++ main_test.go
@@ -14,4 +14,28 @@ func TestUserHandler(t *testing.T) {
 	deleteRequest, _ := http.NewRequest("DELETE", "/user", nil)
 	invalidMethodRequest, _ := http.NewRequest("PUT", "/user", nil)
 
+// create a writer to capture the HTTP response
+	responseRecorder := httptest.NewRecorder()
+
+	// call the UserHandler function with each request and the writer
+	UserHandler(responseRecorder, postRequest)
+	checkResponseStatus(t, responseRecorder, http.StatusOK)
+
+	UserHandler(responseRecorder, getRequest)
+	checkResponseStatus(t, responseRecorder, http.StatusOK)
+
+	UserHandler(responseRecorder, patchRequest)
+	checkResponseStatus(t, responseRecorder, http.StatusOK)
+
+	UserHandler(responseRecorder, deleteRequest)
+	checkResponseStatus(t, responseRecorder, http.StatusOK)
+
+	UserHandler(responseRecorder, invalidMethodRequest)
+	checkResponseStatus(t, responseRecorder, http.StatusMethodNotAllowed)
+}
+
+func checkResponseStatus(t *testing.T, recorder *httptest.ResponseRecorder, expectedStatus int) {
+	if recorder.Result().StatusCode != expectedStatus {
+		t.Errorf("Expected status code %d, but got %d", expectedStatus, recorder.Result().StatusCode)
+	}
 }
