7eabaa5265f1039cb52c27022c4e450a9ed081cf
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-23 14:55:43 +0100

    created upload for creating user pdfs
    upload a file and associate it with a user, this should be a fictional contract - with id

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"user-management/store"
 
 	"github.com/bmizerany/pat"
@@ -14,13 +16,14 @@ func main() {
 	m.Get("/user/:id", http.HandlerFunc(UserHandler))
 	m.Post("/new-user", http.HandlerFunc(newUserHandler))
 
+	http.HandleFunc("/file-upload", fileHandler)
+
 	http.Handle("/", m)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }	
-
 
 
 func UserHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,6 +47,16 @@ func newUserHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintf(w, "Sorry, only POST/GET/PATCH/DELETE methods are supported.")
+	}
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+	uploadFile(w, r)
+	default:
+	w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Sorry, only POST methods are supported.")
 	}
 }
 
@@ -104,4 +117,38 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	fileName := fmt.Sprintf("user-contracts/user-contract-id-%s.pdf", id)
+
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("userContract")
+	if err != nil {
+		fmt.Println("Error retrieving the file")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded file: %+v\n", handler.Filename)
+	fmt.Printf("File size: %+v\n", handler.Size)
+	fmt.Printf("MIME header: %+v\n", handler.Header)
+
+	dst, err := os.Create(fileName)
+	
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+	
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully uploaded file\n")
 }
