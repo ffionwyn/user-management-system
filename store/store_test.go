8ff1a8b7f64b392c14843ccfdcb24432437fd406
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-30 12:12:29 +0100

    added initial unit tests

--- store/store_test.go
+++ store/store_test.go
@@ -1,1 +1,30 @@
 package store
+
+import (
+	"testing"
+)
+
+func TestPerson(t *testing.T) {
+	person := Person{
+		FirstName:  "ffion",
+		SecondName: "griffiths",
+		DOB:        "05/11/1993",
+		Email:      "ffiongriffiths@example.com",
+	}
+	
+	if person.FirstName != "ffion" {
+		t.Errorf("Expected FirstName to be 'ffion', but got '%s'", person.FirstName)
+	}
+
+	if person.SecondName != "griffiths" {
+		t.Errorf("Expected SecondName to be 'griffiths', but got '%s'", person.SecondName)
+	}
+
+	if person.DOB != "05/11/1993" {
+		t.Errorf("Expected DOB to be '05/11/1993', but got '%s'", person.DOB)
+	}
+
+	if person.Email != "ffiongriffiths@example.com" {
+		t.Errorf("Expected Email to be 'ffiongriffiths@example.com', but got '%s'", person.Email)
+	}
+}
